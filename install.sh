d8923bc71b89bf4e03547a3a06af341654dc5cd8
Robin Pfeiffer <contact@rbn.dev>
2021-09-04 01:10:26 +0200

    Add install script

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env sh
+mirage=$(echo "$PWD" | sed 's_/_\\/_g')
+
+sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="mirage"/g' ~/.zshrc
+sed -i -e 's/.*ZSH_CUSTOM=.*/ZSH_CUSTOM='"$mirage"'/g' ~/.zshrc
